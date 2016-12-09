e7b76d795fc15cf90ebe765f585898865645f28c
brittshroyer <britt.shroyer@gmail.com>
2016-12-09 00:21:15 -0600

    almost

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  $('.icon').on('mouseover', function(){
+    this.animate({
+      height: '20px',
+      width: '20px'
+    }, 3000);
+  });
+
+  // $('.icon').animate({ height: '6em', width: '6em' }, 2000, 'linear');
+
+
+});
