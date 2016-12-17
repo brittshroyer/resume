ff7e6f325e5e86c15c32e934f6fefc6b451eb71d
brittshroyer <britt.shroyer@gmail.com>
2016-12-16 23:04:59 -0600

    first

--- js/main.js
+++ js/main.js
@@ -1,13 +1,43 @@
 $(document).ready(function(){
 
-  $('.icon').on('mouseover', function(){
-    this.animate({
-      height: '20px',
-      width: '20px'
-    }, 3000);
+  $('.icon').hover(function () {
+   $(this).toggleClass("result_hover");
   });
+
+
+
+
+  // $('.icon').on('mouseover', function(event){
+  //   $(this).addClass()
+  //
+  //
+  //   $(this).animate({
+  //     height: '80px',
+  //     width: '80px'
+  //   }, 100);
+  // });
+
+
+
+
+
+
+
+
+});
+
 
   // $('.icon').animate({ height: '6em', width: '6em' }, 2000, 'linear');
 
 
-});
+
+
+
+
+
+// $('.icon').on('mouseover', function(event){
+//   $(this).animate({
+//     height: '80px',
+//     width: '80px'
+//   }, 100);
+// });
